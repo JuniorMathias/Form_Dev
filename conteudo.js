172e972a9dd59f1f462969085a7c3e9840253674
juniormathias <juniormathiaas@gmail.com>
2022-11-29 14:22:01 -0300

    válidando a checagem dos inputs na hora de cadastrar e mostrando no alert

--- conteudo.js
+++ conteudo.js
@@ -3,10 +3,11 @@ let email = document.getElementById('email');
 
 
 function cadastrar(){
-    if(nome == ""){
+    if(nome.value.length == 0){
         alert("Preencha o campo NOME");
-    }else if(email == ""){
+    }else if(email.value.length == 0){
         alert("Preencha o campo EMAIL");
-    }
+    }else{
         alert("Cadastro realizado com sucesso!!");
+    }    
 }
