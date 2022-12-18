70fc81805de4fab0b674b55e2601b68d3749a7c5
junior <juniormathiias@gmail.com>
2022-12-18 01:17:49 -0300

    i added some javascript lines and validated the form with the checkboxes

--- conteudo.js
+++ conteudo.js
@@ -1,9 +1,22 @@
 let nome = document.getElementById('nome');
 let email = document.getElementById('email');
+let checkBoxes = document.getElementsByClassName("check");
+var element = document.getElementsByClassName("p1");
 
 
-function cadastrar(){
-    if(nome.value.length != 0 && email.value.length != 0 ){
-        alert("Cadastro realizado com sucesso!!");
-    } 
+function cadastrar(e){
+    if(nome.value.length != 0 && email.value.length != 0  ){
+        checkBoxes = document.getElementsByClassName("check")
+        let noCheckedBoxes = true
+        for (i = 0; i< checkBoxes.length; ++i) {
+            if(checkBoxes[i].checked) {
+                noCheckedBoxes = false
+                alert("Cadastro realizado com sucesso!!" + 
+                " " + (element[0].innerHTML) + " " + nome.value);
+            }
+        }
+        if(noCheckedBoxes) {
+            alert(nome.value + " Você deve selecionar 'Sim' ou 'Não'");
+        }
+    }
 }
